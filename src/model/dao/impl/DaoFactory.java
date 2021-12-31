4d4c6a3fb77c4748b8f619e3971e0ba40d8c04dc
Paloma Oliveira <pfoliveira207@gmail.com>
2021-12-31 16:11:48 -0300

    findById implementation

--- src/model/dao/impl/DaoFactory.java
+++ src/model/dao/impl/DaoFactory.java
@@ -1,5 +1,12 @@
 package model.dao.impl;
 
+import db.DB;
+import model.dao.SellerDao;
+
 public class DaoFactory {
+	
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC(DB.getConnection());
+	}
 
 }
